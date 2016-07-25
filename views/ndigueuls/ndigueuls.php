5c7c194db0154dcfcea69e7d0f7d5790d7440db6
[a621275] Ibrahima khoule <ibrahima.khoule@atos.net>
2016-07-25 19:06:55 +0000

    delete limit 6

--- views/ndigueuls/ndigueuls.php
+++ views/ndigueuls/ndigueuls.php
@@ -73,7 +73,8 @@
                                             </div>
                                         </td>
                                         <td>{{ndigueul.nom}}</td>
-                                        <td>Dieuwrigne {{ndigueul.collecteur}}</td>
+                                        <td><span  ng-if="user.code_profil=='dieuwrigne'
+                                                      || user.code_profil=='universel' || user.code_profil=='cheikh'">Dieuwrigne</span>{{ndigueul.collecteur}}</td>
                                         <td>{{ndigueul.date_debut}}</td>
                                         <td>{{ndigueul.date_fin}}</td>
                                     </tr>
